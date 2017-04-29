8a4e88547e8e15ec091605c9e91007edd90096ff
alenovan <novannanega@gmail.com>
2017-04-29 14:21:47 +0700

    Update dashboard_admin.php

--- application/views/admin/dashboard_admin.php
+++ application/views/admin/dashboard_admin.php
@@ -5,7 +5,7 @@
     	<div class="sidebar-wrapper">
             <div class="logo">
                 <a href="http://www.creative-tim.com" class="simple-text">
-                    sayaa
+                    Alen
                 </a>
             </div>
 
