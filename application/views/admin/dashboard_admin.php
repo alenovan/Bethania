2b6426169ae8c99ab901dabe9d55fd86a35bcb7d
alenovan <novannanega@gmail.com>
2017-04-29 14:25:42 +0700

    edit

--- application/views/admin/dashboard_admin.php
+++ application/views/admin/dashboard_admin.php
@@ -5,7 +5,7 @@
     	<div class="sidebar-wrapper">
             <div class="logo">
                 <a href="http://www.creative-tim.com" class="simple-text">
-                    Alen
+                    Bethania
                 </a>
             </div>
 
